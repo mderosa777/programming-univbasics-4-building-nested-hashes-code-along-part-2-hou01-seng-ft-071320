9753c1917e2f1f0a6b651d360e4f972c06b376af
megan <meggsnbacon777@gmail.com>
2020-07-07 03:24:09 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -18,8 +18,7 @@ def update_status
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }}
-      :additional_characters =>[{name: "Prince Escalus"}{name:"Apothecary"}]
-      epic_tragedy[:additional_characters]<< Prince epic_tragedy[:additional_characters]<<Apothecary
+      
    
 
   epic_tragedy[:montague][:hero][:status]="dead"
